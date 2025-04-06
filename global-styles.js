845b0421b9b26ec9b47e477ac1bc6432db564289
gera-sukaran2000 <gera.sukaran2000@gmail.com>
2025-04-06 19:09:46 +0530

    fourth commit

---
+++ global-styles.js
@@ -0,0 +1,75 @@
+import { StyleSheet, Appearance } from "react-native";
+
+const globalStyles = StyleSheet.create({
+  label16Bold: {
+    fontSize: 16,
+    fontFamily: "KNBold",
+  },
+  label16Medium: {
+    fontSize: 16,
+    fontFamily: "KNMedium",
+  },
+  label16Regular: {
+    fontSize: 16,
+    fontFamily: "KN",
+  },
+  label16Light: {
+    fontSize: 16,
+    fontFamily: "KNLight",
+  },
+
+  label13Bold: {
+    fontSize: 13,
+    fontFamily: "KNBold",
+  },
+  label13Medium: {
+    fontSize: 13,
+    fontFamily: "KNMedium",
+  },
+  label13Regular: {
+    fontSize: 13,
+    fontFamily: "KN",
+  },
+  label13Light: {
+    fontSize: 13,
+    fontFamily: "KNLight",
+  },
+
+  label28Bold: {
+    fontSize: 28,
+    fontFamily: "KNBold",
+  },
+  label28Medium: {
+    fontSize: 28,
+    fontFamily: "KNMedium",
+    lineHeight: 32,
+  },
+  label28Regular: {
+    fontSize: 28,
+    fontFamily: "KN",
+  },
+  label28Light: {
+    fontSize: 28,
+    fontFamily: "KNLight",
+  },
+
+  label24Bold: {
+    fontSize: 24,
+    fontFamily: "KNBold",
+  },
+  label24Medium: {
+    fontSize: 24,
+    fontFamily: "KNMedium",
+  },
+  label24Regular: {
+    fontSize: 24,
+    fontFamily: "KN",
+  },
+  label24Light: {
+    fontSize: 24,
+    fontFamily: "KNLight",
+  },
+  greenPr: { color: "#007B5D" },
+});
+
+export default globalStyles;
